00230392f1be3481d4e803bbfa3f4178088ebd7b
Zakaria Aziz <zaxstyle@hotmail.com>
2014-05-08 23:20:29 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OmrTest::Application.config.secret_key_base = 'a0e35fa37941d60731128245b1bd4b43d921425f6f00a48a535016ea4120194e7ceb0902e16753907f2534c976497b77f1b886c7b534000ff556ede7c1372bc4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OmrTest::Application.config.session_store :cookie_store, key: '_omr_test_session'
